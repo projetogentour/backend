c27ac002df56478308301c1c1af6cee489aa9312
Cristian Wagmaker <cristian.wagmakerpessoal@gmail.com>
2022-03-29 14:59:27 -0300

    Criar o Projeto Spring

---
+++ src/test/java/com/generation/gentour/GentourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.gentour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GentourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
